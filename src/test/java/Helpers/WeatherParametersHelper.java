64217b9b39b130c29ce9a8086301b33416569ab6
Iryna Shymko <shimkoira2011@gmail.com>
2022-05-18 14:43:02 +0200

    homework restAssured lessons 5-6

---
+++ src/test/java/Helpers/WeatherParametersHelper.java
@@ -0,0 +1,7 @@
+package Helpers;
+
+public class WeatherParametersHelper {
+
+    public final static String CLOUDS_ALL = "clouds.all";
+    public final static String NAME = "name";
+}
--- src/test/java/WeatherTest.java
+++ src/test/java/WeatherTest.java
@@ -1,8 +1,10 @@
 import Helpers.RequestSpecificationHelper;
 import Helpers.ResponseSpecificationHelper;
+import Helpers.WeatherParametersHelper;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.is;
 
 public class WeatherTest extends TestBase{
 
@@ -12,5 +14,25 @@ public class WeatherTest extends TestBase{
         when()
                 .get().
         then().spec(ResponseSpecificationHelper.getDefaultResponseSpecification(200));
+    }
+
+    @Test
+    public void shouldCheckCityName(){
+        given(RequestSpecificationHelper.getRequestSpecificationForGetWeather()).
+                when()
+                .get().
+                then()
+                .spec(ResponseSpecificationHelper.getDefaultResponseSpecification(200))
+                .body(WeatherParametersHelper.NAME, is(System.getProperty("cityName")));
+    }
+
+    @Test
+    public void shouldCheckClouds(){
+        given(RequestSpecificationHelper.getRequestSpecificationForGetWeather()).
+                when()
+                .get().
+                then()
+                .spec(ResponseSpecificationHelper.getDefaultResponseSpecification(200))
+                .body(WeatherParametersHelper.CLOUDS_ALL, is(Integer.parseInt(System.getProperty("cloudsLevel"))));
     }
 }
